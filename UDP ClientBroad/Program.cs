112418f6b3481eda9853525032771212b6668e87
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-05-05 12:36:58 +0200

    Added ClientBroad and ServerBroad

---
+++ UDP ClientBroad/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace UDP_ClientBroad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpReceiver = new UdpClient(7000);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 7000);
+            Console.WriteLine("Receiver is blocked");
+
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("http://anbo-bookstorerest.azurewebsites.net/api/Books/");
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+
+
+            try
+            {
+                while (true)
+                {
+                    Byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("Recived: " + receivedData.ToString());
+
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("https://shoppersizerrest.azurewebsites.net/api/LiveNumber/");
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        string stringToSend = "{\"number\":" + int.Parse(receivedData) + "}";
+
+                        var response = client.PutAsync("1", new StringContent(stringToSend, Encoding.UTF8, "application/json")).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.Write("Success");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error");
+                            //Console.WriteLine(stringToSend);
+                            //Console.WriteLine(response.StatusCode);
+                            //Console.WriteLine(response.RequestMessage);
+                        }
+
+                    }
+
+
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ UDP ServerBroad/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDP_ServerBroad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            UdpClient udpServer = new UdpClient(0);
+            udpServer.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 7000);
+
+            Console.WriteLine("Broadcast started: Press Enter");
+            Console.ReadLine();
+
+            while (true)
+            {
+                int number = rand.Next(-1, 2);
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(number.ToString());
+                try
+                {
+                    udpServer.Send(sendBytes, sendBytes.Length, endPoint);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Console.WriteLine(" " + number);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
