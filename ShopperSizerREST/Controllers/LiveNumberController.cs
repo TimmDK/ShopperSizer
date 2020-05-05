4347fdcff893c227b4a183914b01a30ca2d5c4b7
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-05-05 11:37:41 +0200

    Added Put and Get  to rest

---
+++ ShopperSizerREST/Controllers/LiveNumberController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopperSizerREST.Models;
+
+namespace ShopperSizerREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiveNumberController : ControllerBase
+    {
+        //private static int _idCounter = 1;
+        private static List<LiveNumber> numbers = new List<LiveNumber>()
+        {
+            new LiveNumber(1,5), new LiveNumber(2,10)
+        };
+        
+        // GET: api/LiveNumber
+        [HttpGet]
+        public List<LiveNumber> Get()
+        {
+            return numbers;
+        }
+
+        // GET: api/LiveNumber/5
+        [HttpGet("{id}", Name = "Get")]
+        public LiveNumber Get(int id)
+        {
+            return numbers.Find(i => i.Id == id);
+        }
+
+        // POST: api/LiveNumber
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/LiveNumber/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] LiveNumber value)
+        {
+            LiveNumber i = Get(id);
+            if(i != null)
+            {
+                i.Number = value.Number;
+                i.Id = value.Id;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ShopperSizerREST/Models/LiveNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopperSizerREST.Models
+{
+    public class LiveNumber
+    {
+        private int _number;
+        private int _id;
+
+        public LiveNumber(int id, int number)
+        {
+            _id = id;
+            _number = number;
+        }
+
+        public LiveNumber()
+        {
+
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public int Number { get => _number; set => _number = value; }
+
+
+    }
+}
