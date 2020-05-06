2362c43c544ac97843c39774eaad78c3559b3cc8
Carl Guarin Dreslet <carl0572@edu.easj.dk>
2020-05-06 10:22:26 +0200

    ShopperSizeTest
    -Carl

---
+++ ShopperSizeTest/ShopperSizeRest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopperSizerREST.Controllers;
+using ShopperSizerREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopperSizeTest
+{
+    [TestClass]
+    class ShopperSizeRest
+    {
+        [TestMethod] 
+        public void GetAllNumbers()
+        {
+            //Arange
+            List<LiveNumber> list = new List<LiveNumber>() { new LiveNumber(1, 1) };
+            
+            LiveNumberController ln = new LiveNumberController();
+           
+            List<LiveNumber> result = ln.Get();
+            //Act
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(list.Count, result.Count);
+        }
+        
+    }
+}
