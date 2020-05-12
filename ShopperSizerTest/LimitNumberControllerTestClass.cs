3b61561fb853ba3570de4256bde7108931cb199d
jaco6002 <jaco6002@edu.easj.dk>
2020-05-12 13:25:18 +0200

    limitnumber test

---
+++ ShopperSizerTest/LimitNumberControllerTestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopperSizerREST.Controllers;
+using ShopperSizerREST.Models;
+
+namespace ShopperSizerTest
+{
+    [TestClass]
+    public class LimitNumberControllerTestClass
+    {
+        private LimitNumberController lnc;
+
+        [TestInitialize]
+        public void init()
+        {
+            lnc = new LimitNumberController();
+        }
+
+        [TestMethod]
+        public void TestPut()
+        {
+            int expected = 50;
+            LimitNumber ln= new LimitNumber(1,expected);
+            var result = lnc.Put(1, ln);
+            Assert.AreEqual(expected,result.LimitTal);
+        }
+        [TestMethod]
+        public void TestGet()
+        {
+            int expected = 200000000;
+            LimitNumber ln = new LimitNumber(1, expected);
+            lnc.Put(1,ln);
+            int actual = lnc.Get()[0].LimitTal;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
