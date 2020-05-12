f567173d582cce19c4444e5d66833abf0fbdad43
jaco6002 <jaco6002@edu.easj.dk>
2020-05-12 13:05:40 +0200

    made a test that doesnt work :)

--- ShopperSizerTest/DataSetsControllerTestClass.cs
+++ ShopperSizerTest/DataSetsControllerTestClass.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopperSizerREST.Controllers;
+using ShopperSizerREST.Models;
 
 namespace ShopperSizerTest
 {
     [TestClass]
     public class DataSetsControllerTestClass
     {
+        private DataSetsController _controller;
+        //vi kan ikke teste hen over en database, så vi har besluttet os for ikke at unit teste denne controller
         [TestInitialize]
         public void testInit()
         {
-
+            //_controller = new DataSetsController(new DataSetContext(new DbContextOptions<DataSetContext>()));
+            //vi kan ikke teste hen over en database, så vi har besluttet os for ikke at unit teste denne controller
         }
-        [TestMethod]
-        public void test()
-        {
+        //[TestMethod]
+        //public async Task test()
+        //{
+        //    //vi kan ikke teste hen over en database, så vi har besluttet os for ikke at unit teste denne controller
+        //    DataSet set1 = new DataSet();
+        //    set1.Count = 50;
+        //    var result = await _controller.PostDataSet(set1);
+        //    Assert.AreEqual(50,result.Value.Count);
+        //}
 
-        }
+        //vi kan ikke teste hen over en database, så vi har besluttet os for ikke at unit teste denne controller
+
     }
 }
