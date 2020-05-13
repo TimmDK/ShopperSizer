981d46f07979f2c947bb170d8fac0cea84b7cb2f
jaco6002 <jaco6002@edu.easj.dk>
2020-05-13 12:35:29 +0200

    the ui test is now working and passed

--- ShopperSizerTest/UITest.cs
+++ ShopperSizerTest/UITest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -29,24 +30,28 @@ namespace ShopperSizerTest
         [TestMethod]
         public void TestMethod1()
         {
+            string expected = "200";
+
             _driver.Navigate().GoToUrl("http://localhost:3000/chartview.htm");
-            Assert.AreEqual("Chart View",_driver.Title);
+            Assert.AreEqual("Chart View - Shopper Sizer",_driver.Title);
 
-            IWebElement inputElement1 = _driver.FindElement(By.Id("test"));
-            inputElement1.SendKeys("500");
+            IWebElement inputElement1 = _driver.FindElement(By.Id("valueInput"));
+            inputElement1.SendKeys(expected);
 
-            IWebElement buttonElement = _driver.FindElement(By.Id("SaveButton"));
+            IWebElement buttonElement = _driver.FindElement(By.Id("saveButton"));
             buttonElement.Click();
 
-            IWebElement outputElement = _driver.FindElement(By.Id("LiveNumber"));
+            Thread.Sleep(3000);
+
+            IWebElement outputElement = _driver.FindElement(By.Id("liveNumber"));
             int Livenumber = int.Parse(outputElement.Text);
 
-            IWebElement outputElement2 = _driver.FindElement(By.Id("Remaining"));
+            IWebElement outputElement2 = _driver.FindElement(By.Id("remaining"));
             int Remaining = int.Parse(outputElement2.Text);
 
             int result = Livenumber + Remaining;
 
-            Assert.AreEqual(500,result);
+            Assert.AreEqual(int.Parse(expected),result);
         }
     }
 }
