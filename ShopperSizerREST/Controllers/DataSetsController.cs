f9b0f00a09a913777333f8a6c61b2044a67c9ffc
ande202d <48085313+ande202d@users.noreply.github.com>
2020-05-13 13:17:50 +0200

    added search date option

--- ShopperSizerREST/Controllers/DataSetsController.cs
+++ ShopperSizerREST/Controllers/DataSetsController.cs
@@ -18,6 +18,24 @@ namespace ShopperSizerREST.Controllers
         public DataSetsController(DataSetContext context)
         {
             _context = context;
+            if (_context.DataSets.Count() == 0)
+            {
+                DataSet d1 = new DataSet(); d1.Date = DateTime.Now.Subtract(TimeSpan.FromHours(1)); d1.Count = 10;
+                DataSet d2 = new DataSet(); d2.Date = DateTime.Now.Subtract(TimeSpan.FromHours(2)); d2.Count = 20;
+                DataSet d3 = new DataSet(); d3.Date = DateTime.Now.Subtract(TimeSpan.FromHours(3)); d3.Count = 30;
+                DataSet d4 = new DataSet(); d4.Date = DateTime.Now.Subtract(TimeSpan.FromHours(4)); d4.Count = 40;
+                DataSet d5 = new DataSet(); d5.Date = DateTime.Now.Subtract(TimeSpan.FromHours(4)).Add(TimeSpan.FromMinutes(2)); d5.Count = 80;
+                DataSet d6 = new DataSet(); d6.Date = DateTime.Now.Subtract(TimeSpan.FromDays(1)).Add(TimeSpan.FromMinutes(2)); d6.Count = 90;
+                DataSet d7 = new DataSet(); d7.Date = DateTime.Now.Subtract(TimeSpan.FromDays(2)).Add(TimeSpan.FromMinutes(2)); d7.Count = 100;
+                _context.DataSets.Add(d1);
+                _context.DataSets.Add(d2);
+                _context.DataSets.Add(d3);
+                _context.DataSets.Add(d4);
+                _context.DataSets.Add(d5);
+                _context.DataSets.Add(d6);
+                _context.DataSets.Add(d7);
+                _context.SaveChanges();
+            }
         }
 
         // GET: api/DataSets
@@ -40,6 +58,47 @@ namespace ShopperSizerREST.Controllers
 
             return dataSet;
         }
+
+        //GET: api/DataSets/date/
+        [HttpGet]
+        [Route("date/{date}")]
+        public List<DataSet> GetDataSetFromDate(string date)
+        {
+            List<DataSet> listToReturn = new List<DataSet>();
+            List<DataSet> listToWorkWith = new List<DataSet>();
+
+            foreach (DataSet d in GetDataSets().Result.Value)
+            {
+                if (d.Date.Date == DateTime.Parse(date))
+                {
+                    listToWorkWith.Add(d);
+                }
+            }
+
+            DateTime dummy = DateTime.MinValue;
+            int testHour = 1;
+            int[] testHours = new[]
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+
+            foreach (int i in testHours)
+            {
+                List<DataSet> dummyList = listToWorkWith.FindAll(e => e.Date.Hour == i);
+                DateTime dat = DateTime.MinValue;
+                int idd = -1;
+                foreach (DataSet d in dummyList)
+                {
+                    if (d.Date > dat)
+                    {
+                        dat = d.Date;
+                        idd = d.Id;
+                    }
+                }
+                listToReturn.Add(listToWorkWith.Find(e=>e.Id == idd));
+            }
+
+            return listToReturn;
+        }
+
         /*
         // PUT: api/DataSets/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
@@ -72,7 +131,7 @@ namespace ShopperSizerREST.Controllers
 
             return NoContent();
         }*/
-        
+
 
         // POST: api/DataSets
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
