3477943e1db6a07a6182d220039dd7e48492f13b
jaco6002 <jaco6002@edu.easj.dk>
2020-05-14 11:15:21 +0200

    test af usercontroller

---
+++ ShopperSizerTest/UsersControllerTestClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopperSizerREST.Controllers;
+using ShopperSizerREST.Models;
+
+namespace ShopperSizerTest
+{
+    [TestClass]
+    public class UsersControllerTestClass
+    {
+        private UsersController usersController;
+
+        [TestInitialize]
+        public void init()
+        {
+            usersController = new UsersController();
+        }
+
+        [TestMethod]
+        public void TestPutUsername()
+        {
+            string expected = "test";
+            User user = new User(1,expected,"password");
+            var result = usersController.Put(1, user);
+            Assert.AreEqual(expected, result.Username);
+        }
+
+        [TestMethod]
+        public void TestPutPassword()
+        {
+            string expected = "test";
+            User user = new User(1, "username", expected);
+            var result = usersController.Put(1, user);
+            Assert.AreEqual(expected, result.Password);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            string expected = "expected";
+            User user = new User(1, expected,"password");
+            usersController.Put(1, user);
+            string actual = usersController.Get()[0].Username;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
