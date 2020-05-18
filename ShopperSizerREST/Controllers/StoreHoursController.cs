9f82e8c4b6b8229b87b1b555f0d3caee4624c80c
ande202d <48085313+ande202d@users.noreply.github.com>
2020-05-18 12:00:03 +0200

    added PublicOrNot added StoreHours

---
+++ ShopperSizerREST/Controllers/StoreHoursController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopperSizerREST.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopperSizerREST.Controllers
+{
+    [Route("api/[controller]")]
+    public class StoreHoursController : Controller
+    {
+        private static StoreHours Hours = new StoreHours(1, 22);
+
+        // GET: api/<controller>
+        [HttpGet]
+        public StoreHours Get()
+        {
+            return Hours;
+        }
+
+        // PUT api/<controller>/5
+        //[HttpPut]
+        //public StoreHours Put([FromBody]StoreHours value)
+        //{
+        //    Hours = value;
+        //    return Get();
+        //}
+        [HttpPut]
+        [Route("open/{hour}")]
+        public StoreHours PutOpen(int hour)
+        {
+            Hours.OpeningHour = hour;
+            return Get();
+        }
+        [HttpPut]
+        [Route("close/{hour}")]
+        public StoreHours PutClose(int hour)
+        {
+            Hours.ClosingHour = hour;
+            return Get();
+        }
+
+    }
+}
---
+++ ShopperSizerREST/Models/StoreHours.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopperSizerREST.Models
+{
+    public class StoreHours
+    {
+        private int _openingHour;
+        private int _closingHour;
+
+        public StoreHours()
+        {
+
+        }
+        public StoreHours(int a, int b)
+        {
+            _openingHour = a;
+            _closingHour = b;
+        }
+
+        public int OpeningHour
+        {
+            get => _openingHour;
+            set
+            {
+                if (value >= 0 && value <= 23 && value < ClosingHour) _openingHour = value;
+            }
+        }
+
+        public int ClosingHour
+        {
+            get => _closingHour;
+            set
+            {
+                if (value >= 0 && value <= 23 && value > OpeningHour) _closingHour = value;
+            }
+        }
+    }
+}
